35b9e3881c8fbdb320055ca5f28f47f6ee0b2841
Sabrina <sabrina.tribute@gmail.com>
2023-06-29 14:50:56 -0300

    Initial commit

---
+++ src/main/java/Word.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = word();
+        System.out.println(string);
+
+    }
+
+    public static String word() {
+        return "A string of your choice.";
+    }
+}
